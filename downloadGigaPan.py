490ccf414d7341a087cd25d6c8bba52f4c8db65a
Fritz Lekschas <service@lekschas.de>
2018-01-10 11:09:43 +0100

    Download tiles from all zoom levels. Create a tile set info file. Fix a bug regarding the stream-based download. Allow skipping existing files

--- downloadGigaPan.py
+++ downloadGigaPan.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # usage: python downloadGigaPan.py <photo-id> <start-height> <start-width>
 # http://gigapan.org/gigapans/<photo-id>
 
@@ -6,6 +8,8 @@ import os
 import math
 import shutil
 import requests
+import json
+import pathlib
 
 from multiprocessing import Pool
 from xml.dom.minidom import parseString
@@ -32,29 +36,40 @@ def find_element_value(e, name):
     return None
 
 
-def d(i, j, s):
-    filename = "%04d-%04d.jpg" % (i, j)
+def d(i, j, z, s):
+    filename = "%d.%d.%d.jpg" % (z, j, i)
     url = "%s/get_ge_tile/%d/%d/%d/%d" % (
-        base, photo_id, maxlevel, j, i
+        base, photo_id, z, j, i
     )
 
-    print(url, filename)
+    tile = 'downloads/{}/tiles/{}'.format(photo_id, filename)
+    downloaded = False
+
+    if (
+        not os.path.isfile(tile) or
+        os.path.getsize(tile) == 0
+    ):
+        try:
+            r = s.get(url, stream=True)
+        except requests.exceptions.ConnectionError as e:
+            # Needs improvement
+            print(
+                'Failed to download zoom:{} width:{}  height:{}. Retry!'
+                .format(z, i, j)
+            )
+            sys.stdout.flush()
+            with requests.Session() as s:
+                r = s.get(url, stream=True)
+
+        if r.status_code == 200:
+            with open(tile, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+
+            downloaded = True
+
+    print(url, filename, 'downloaded' if downloaded else 'skipped')
     sys.stdout.flush()
-
-    try:
-        h = s.get(url, stream=True)
-    except requests.exceptions.ConnectionError as e:
-        # Needs improvement
-        print(
-            'Failed to download width:{}  height:{}. Retry with new session!'
-            .format(i, j)
-        )
-        sys.stdout.flush()
-        with requests.Session() as s:
-            h = s.get(url, stream=True)
-
-    with open('downloads/{}/{}'.format(photo_id, filename), 'wb') as f:
-        shutil.copyfileobj(h.raw, f)
 
 
 # main
@@ -66,34 +81,61 @@ if not os.path.exists(str(photo_id)):
 base = 'http://www.gigapan.org'
 
 # read the kml file
-h = requests.get('{}/gigapans/{}.kml'.format(base, photo_id))
-dom = parseString(h.text)
+r = requests.get('{}/gigapans/{}.kml'.format(base, photo_id))
+dom = parseString(r.text)
 
 arglen = len(sys.argv)
 start_height = int(sys.argv[2]) if arglen >= 3 else 0
 start_width = int(sys.argv[3]) if arglen >= 4 else 0
+start_zoom = int(sys.argv[4]) if arglen >= 5 else 0
 
 # find the width and height, level
 height = int(find_element_value(dom.documentElement, 'maxHeight'))
 width = int(find_element_value(dom.documentElement, 'maxWidth'))
 tile_size = int(find_element_value(dom.documentElement, 'tileSize'))
-print(width, height, tile_size, start_width, start_height)
 
-maxlevel = max(math.ceil(width/tile_size), math.ceil(height/tile_size))
-maxlevel = int(math.ceil(math.log(maxlevel) / math.log(2.0)))
-wt = int(math.ceil(width/tile_size)) + 1
-ht = int(math.ceil(height/tile_size)) + 1
-print(ht, wt, maxlevel)
+max_zoom = max(math.ceil(width/tile_size), math.ceil(height/tile_size))
+max_zoom = int(math.ceil(math.log(max_zoom) / math.log(2.0)))
+print(width, height, tile_size, start_width, start_height, max_zoom)
 
-# loop around to get every tile
-for j in range(start_height, ht):
-    with requests.Session() as s:
-        try:
-            with Pool(processes=4) as pool:
-                pool.starmap(
-                    d, zip(range(start_width, wt), repeat(j), repeat(s))
-                )
-        except requests.exceptions.ConnectionError as e:
-            pass
-    with open('last.txt', 'w') as f:
-        f.write('height: {}'.format(j))
+pathlib.Path(
+    'downloads/{}/tiles'.format(photo_id)
+).mkdir(parents=True, exist_ok=True)
+
+# Get annotations
+r = requests.get('{}/gigapans/{}/snapshots.json'.format(base, photo_id))
+with open('downloads/{}/snapshots.json'.format(photo_id), 'w') as f:
+    f.write(r.text)
+
+# Write tile set infos
+with open('downloads/{}/info.json'.format(photo_id), 'w') as f:
+    json.dump({
+        "tile_size": tile_size,
+        "max_width": width,
+        "max_height": height,
+        "max_zoom": max_zoom
+    }, f)
+
+# loop over all zoom levels
+for z in range(start_zoom, max_zoom + 1):
+    div = 2 ** (max_zoom - z)
+    wt = int(math.ceil((width / div) / tile_size))
+    ht = int(math.ceil((height / div) / tile_size))
+    print(z, wt, ht)
+    for j in range(start_height, ht):
+        with requests.Session() as s:
+            try:
+                with Pool(processes=4) as pool:
+                    pool.starmap(
+                        d,
+                        zip(
+                            range(start_width, wt),
+                            repeat(j),
+                            repeat(z),
+                            repeat(s)
+                        )
+                    )
+            except requests.exceptions.ConnectionError as e:
+                pass
+        with open('last.txt', 'w') as f:
+            f.write('zoom: {}; height: {}'.format(z, j))
